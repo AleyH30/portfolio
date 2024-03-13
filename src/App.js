0a17c1a6f781434b7084eb5308a5d5f897e6a1fa
Alexis Hall <135035822+AleyH30@users.noreply.github.com>
2024-03-12 22:47:09 -0700

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Projects from './Pages/Projects/Projects';
 import Contact from './Pages/Contact/Contact';
@@ -8,14 +8,14 @@ import Navbar from './Pages/0Components/Navbar/Navbar';
 
 const App = () => {
     return (
-            <BrowserRouter>
+            <HashRouter>
                 <Navbar/>
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/projects' element={<Projects />} />
                     <Route path='/contact' element={<Contact />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
     )
 }
 
