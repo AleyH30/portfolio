9fce4018e2d20a78592510330e7e26cbc0bbadb4
Alexis Hall <135035822+AleyH30@users.noreply.github.com>
2024-03-18 04:30:51 -0700

    update page

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -46,7 +46,7 @@ const Projects = () => {
                             <p className='project-desc-header'>Flare</p>
                             <p className='project-desc-par'>An e-commerce website for clothing apparel. This front-end, fully responsive website features a functional filter system
                                 that allows user to sort by price, color, and size, full cart functionality including the ability to remove or increase quantity,
-                                sliders to showcase products and a fully working single product display that allows user to
+                                sliders to showcase products and a fully working single product display that allows users to smoothly
                                 switch between product colors and images.
                             </p>
                             <div className='project-links'>
@@ -71,7 +71,7 @@ const Projects = () => {
                     <div className='res-wrapper'>
                         <div className='project-desc'>
                             <p className='project-desc-header'>Pinela's Eatery</p>
-                            <p className='project-desc-par'>A business website for an elite restaurant that features an expandable restaurant menu, image gallery, and smooth scrolling.
+                            <p className='project-desc-par'>A business website for an elite restaurant that features an expandable restaurant menu, image gallery, and other details that enhance user experience.
                                 This front-end, fully responsive website also features a reservation section that integrates the MUI API date and time selection feature and a contact section 
                                 that allows for customer inquiries to be set-up.
                             </p>
